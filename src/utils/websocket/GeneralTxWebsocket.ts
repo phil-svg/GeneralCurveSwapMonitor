9580ed293a5557fcdca2936206ff08e27f73e77d
phil <phil.softwaredev@protonmail.com>
2025-01-25 20:39:02 +0100

    whitelisted crv2pool

--- src/utils/websocket/GeneralTxWebsocket.ts
+++ src/utils/websocket/GeneralTxWebsocket.ts
@@ -1,14 +1,14 @@
-import { EnrichedTransactionDetail } from "../../Interfaces.js";
-import { buildGeneralTransactionMessage } from "../telegram/TelegramBot.js";
-import { io } from "socket.io-client";
-import { priceTransaction } from "../txValue/PriceTransaction.js";
-import { FILTER_VALUE_DEXDEX, url } from "../../GeneralSwapMonitor.js";
-import { solverLabels } from "../whitelisting/Whitelist.js";
+import { EnrichedTransactionDetail } from '../../Interfaces.js';
+import { buildGeneralTransactionMessage } from '../telegram/TelegramBot.js';
+import { io } from 'socket.io-client';
+import { priceTransaction } from '../txValue/PriceTransaction.js';
+import { FILTER_VALUE_DEXDEX, url } from '../../GeneralSwapMonitor.js';
+import { solverLabels } from '../whitelisting/Whitelist.js';
 
 const processedTxIds = new Set();
 
-import fs from "fs";
-import { promisify } from "util";
+import fs from 'fs';
+import { promisify } from 'util';
 
 // Promisify the necessary fs methods for easier async/await usage
 export const writeFileAsync = promisify(fs.writeFile);
@@ -19,7 +19,7 @@ async function saveLastSeenToFile(hash: string, timestamp: Date) {
     txHash: hash,
     txTimestamp: timestamp.toISOString(),
   };
-  await writeFileAsync("lastSeen.json", JSON.stringify(data, null, 2));
+  await writeFileAsync('lastSeen.json', JSON.stringify(data, null, 2));
 }
 
 // Clear the cache every 5 minutes
@@ -33,17 +33,17 @@ export async function connectToWebsocket(eventEmitter: any) {
   // A map to store transactions that are being delayed
   const pendingTransactions = new Map();
 
-  mainSocket.on("connect", () => {
-    console.log("connected");
+  mainSocket.on('connect', () => {
+    console.log('connected');
     // Connect to both General Tx Livestream and Sandwich Livestream
-    mainSocket.emit("connectToGeneralTxLivestream");
-    mainSocket.emit("connectToGeneralSandwichLivestream");
+    mainSocket.emit('connectToGeneralTxLivestream');
+    mainSocket.emit('connectToGeneralSandwichLivestream');
 
-    mainSocket.on("NewSandwich", async (enrichedSandwich: any) => {
-      console.log("Received result: ", enrichedSandwich);
+    mainSocket.on('NewSandwich', async (enrichedSandwich: any) => {
+      console.log('Received result: ', enrichedSandwich);
       // Remove related transactions from the pendingTransactions map
       // by looking for transactions from the frontrun, center, and backrun parts of the sandwich
-      ["frontrun", "backrun"].forEach((type) => {
+      ['frontrun', 'backrun'].forEach((type) => {
         const hash = enrichedSandwich[type].tx_hash;
         if (pendingTransactions.has(hash)) {
           clearTimeout(pendingTransactions.get(hash));
@@ -58,13 +58,13 @@ export async function connectToWebsocket(eventEmitter: any) {
       });
     });
 
-    mainSocket.on("NewGeneralTx", async (enrichedTransaction: EnrichedTransactionDetail) => {
+    mainSocket.on('NewGeneralTx', async (enrichedTransaction: EnrichedTransactionDetail) => {
       // Saving last-seen values, in case bot goes quite.
       let lastSeenTxHash = enrichedTransaction.tx_hash;
       let lastSeenTxTimestamp = new Date();
       await saveLastSeenToFile(lastSeenTxHash, lastSeenTxTimestamp);
 
-      console.log("enrichedTransaction", enrichedTransaction);
+      console.log('enrichedTransaction', enrichedTransaction);
 
       // Check if the transaction has already been processed
       if (processedTxIds.has(enrichedTransaction.tx_id)) return;
@@ -77,7 +77,10 @@ export async function connectToWebsocket(eventEmitter: any) {
         // Process the transaction after 5 seconds, unless it's part of a sandwich transaction
 
         // If no coins were moved in the transaction, there's nothing to process
-        if (enrichedTransaction.coins_leaving_wallet.length === 0 && enrichedTransaction.coins_entering_wallet.length === 0) {
+        if (
+          enrichedTransaction.coins_leaving_wallet.length === 0 &&
+          enrichedTransaction.coins_entering_wallet.length === 0
+        ) {
           console.log(`No Coins were moved in tx ${enrichedTransaction.tx_hash}`);
           return;
         }
@@ -90,12 +93,21 @@ export async function connectToWebsocket(eventEmitter: any) {
 
         const whitelistedAddresses = solverLabels.map((solver) => solver.Address.toLowerCase());
 
+        const address_crv2pool = '0x4f493b7de8aac7d55f71853688b1f7c8f0243c85';
+
         if (value) {
-          if (value < FILTER_VALUE_DEXDEX && !whitelistedAddresses.includes(enrichedTransaction.poolAddress.toLowerCase())) {
+          if (enrichedTransaction.poolAddress.toLowerCase() === address_crv2pool.toLowerCase()) {
+            const message = await buildGeneralTransactionMessage(enrichedTransaction, value);
+            eventEmitter.emit('newMessage', message);
+            return;
+          } else if (
+            value < FILTER_VALUE_DEXDEX &&
+            !whitelistedAddresses.includes(enrichedTransaction.poolAddress.toLowerCase())
+          ) {
             return;
           }
           const message = await buildGeneralTransactionMessage(enrichedTransaction, value);
-          eventEmitter.emit("newMessage", message);
+          eventEmitter.emit('newMessage', message);
         } else {
           console.log(`Couldn't price transaction ${enrichedTransaction.tx_hash}`);
         }
