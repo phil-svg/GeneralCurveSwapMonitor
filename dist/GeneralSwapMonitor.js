a0b0584ef64ac6dcec284d620abab98d1e1bfead
phil <phil.softwaredev@protonmail.com>
2025-08-04 14:27:55 +0200

    bug fixes

--- dist/GeneralSwapMonitor.js
+++ dist/GeneralSwapMonitor.js
@@ -1,6 +1,9 @@
 import { EventEmitter } from 'events';
 import { telegramBotMain } from './utils/telegram/TelegramBot.js';
+import { connectToWebsocket } from './utils/websocket/GeneralTxWebsocket.js';
 import { startFeeDistributor } from './utils/feeDistributor/FeeDistributorMain.js';
+import { startCrvUSDFlashloan } from './utils/crvUSD_Flashloan/Flashloan.js';
+import { startFeeSplitter } from './utils/feeSplitter/FeeSplitterMain.js';
 console.clear();
 const ENV = 'prod';
 // const ENV = 'test';
@@ -11,10 +14,10 @@ export const FILTER_VALUE_DEXDEX = 1000000;
 const eventEmitter = new EventEmitter();
 async function main() {
     await telegramBotMain(ENV, eventEmitter);
-    // await connectToWebsocket(eventEmitter);
+    await connectToWebsocket(eventEmitter);
     await startFeeDistributor(eventEmitter);
-    // await startCrvUSDFlashloan(eventEmitter);
-    // await startFeeSplitter(eventEmitter);
+    await startCrvUSDFlashloan(eventEmitter);
+    await startFeeSplitter(eventEmitter);
 }
 await main();
 //# sourceMappingURL=GeneralSwapMonitor.js.map
